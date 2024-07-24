ed84ace2835530eb5e5d7151c258566a910ed4d4
TomasOndrisak <tomas.ondrisak@gmail.com>
2024-07-24 23:00:51 +0200

    added navbar and dummy modules

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './modules/home/home.component';
+import { DiaryComponent } from './modules/diary/diary.component';
+import { AccountComponent } from './modules/account/account.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'diary',
+        component: DiaryComponent
+    },
+    {
+        path: 'account',
+        component: AccountComponent
+    }
+];
--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { AccountComponent } from '../../modules/account/account.component';
+import { DiaryComponent } from '../../modules/diary/diary.component';
+import { HomeComponent } from '../../modules/home/home.component';
+import { Router, RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatButtonModule, MatToolbarModule],
+  imports: [MatButtonModule, MatToolbarModule, AccountComponent, DiaryComponent, HomeComponent, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
