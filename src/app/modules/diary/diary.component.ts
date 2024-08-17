3610bf20ebd77e105109b1fa6a18006964700d15
TomasOndrisak <tomas.ondrisak@gmail.com>
2024-08-17 22:38:33 +0200

    Diary page

--- src/app/modules/diary/diary.component.ts
+++ src/app/modules/diary/diary.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 @Component({
   selector: 'app-diary',
   standalone: true,
-  imports: [],
+  imports: [MatInputModule, MatFormFieldModule],
   templateUrl: './diary.component.html',
   styleUrl: './diary.component.scss'
 })
 export class DiaryComponent {
 
+  constructor() { }
+  diary = new FormGroup({});
 }
