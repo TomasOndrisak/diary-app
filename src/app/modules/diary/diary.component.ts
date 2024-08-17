765d98369e30f505779f4c1cf8599d8b5541118f
TomasOndrisak <tomas.ondrisak@gmail.com>
2024-08-17 23:03:56 +0200

    Added date of writting

--- src/app/modules/diary/diary.component.ts
+++ src/app/modules/diary/diary.component.ts
@@ -1,17 +1,22 @@
 import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-diary',
   standalone: true,
-  imports: [MatInputModule, MatFormFieldModule],
+  imports: [MatInputModule, MatFormFieldModule, DatePipe],
   templateUrl: './diary.component.html',
   styleUrl: './diary.component.scss'
 })
 export class DiaryComponent {
 
   constructor() { }
-  diary = new FormGroup({});
+
+  DateOfWritting : Date = new Date();
+  DaysOfWeekArray : string[] = ['Pondelok', 'Utorok', 'Streda', 'Štvrtok', 'Piatok', 'Sobota', 'Nedeľa'];
+
+  public getDayName = () : string => { return this.DaysOfWeekArray[this.DateOfWritting.getDay() - 1]; }
+
 }
