8fa6fd27e8b4f93053ad361a7598bb7f6a18a8b5
TomasOndrisak <tomas.ondrisak@gmail.com>
2024-08-19 22:17:20 +0200

    generated inventory component and added route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/home.component';
 import { DiaryComponent } from './modules/diary/diary.component';
 import { AccountComponent } from './modules/account/account.component';
+import { InventoryComponent } from './modules/inventory/inventory.component';
 
 export const routes: Routes = [
     {
@@ -15,5 +16,9 @@ export const routes: Routes = [
     {
         path: 'account',
         component: AccountComponent
+    },
+    {
+        path: 'inventory',
+        component: InventoryComponent
     }
 ];
